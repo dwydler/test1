80d49f4430cb0286d99ca4e1a95ec5ff1b604aa5
Blackmoon <dwydler@users.noreply.github.com>
2025-06-09 13:18:45 +0200

    Create Generate-SBOM.sh

---
+++ images/ubuntu/scripts/docs-gen/Generate-SBOM.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+################################################################################
+##  File:  Generate-SBOM.sh
+##  Desc:  Create SBOM for the release
+################################################################################
+
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/install.sh
+
+# Install sysft
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
+
+# Generating SBOM 
+syft dir:/ -vv -o spdx-json=sbom.json
+
+# Preparing artifact (raw SBOM.json is too big)
+zip sbom.json.zip sbom.json
